2b974f68cd5d5920408bb3e30ec8ff6ae086f76d
Cody Ring Rissler <cody.ring.rissler@gmail.com>
2020-09-04 11:42:19 -0700

    Jupyter notebook now makes grpahs that aren't in tableau

--- Download_Nyt.py
+++ Download_Nyt.py
@@ -6,17 +6,19 @@ nyt = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/maste
                   , index_col='date')  # index_col='date'
 
 
-nyt = pd.DataFrame(nyt[nyt['state'].isin(['Washington'])]).sort_values(by='date', ascending=False)
+WA = pd.DataFrame(nyt[nyt['state'].isin(['Washington'])]).sort_values(by='date', ascending=False)
 
+WA['cases_day'] = abs(WA.cases.diff(periods=-1))
+WA['case_curve'] = WA.cases_day.rolling(window=14).mean()
 
-nyt['cases_day'] = abs(nyt.cases.diff(periods=-1))
-nyt['case_curve'] = nyt.cases_day.rolling(window=14).mean()
-
-nyt['deaths_day'] = abs(nyt.deaths.diff(periods=-1))
-nyt['death_curve'] = nyt.deaths_day.rolling(window=14).mean()
+WA['deaths_day'] = abs(WA.deaths.diff(periods=-1))
+WA['death_curve'] = WA.deaths_day.rolling(window=14).mean()
 
 # Day Zero https://www.nytimes.com/2020/03/11/business/media/tom-hanks-coronavirus.html
-nyt = nyt.loc[nyt.index >= '3/11/2020']
+WA = WA.loc[WA.index >= '3/11/2020']
 
 
-nyt.to_csv("C:/Users/User/Documents/My Tableau Repository/Datasources/NYT_Washington.csv")
+
+
+WA.to_csv("C:/Users/User/Documents/My Tableau Repository/Datasources/NYT_Washington.csv")
+nyt.to_csv("C:/Users/User/Source/Washington_Corona/Data/NYT_All.csv")
